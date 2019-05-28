d25760b26f360f90fb65d1d012a1f26fb8cc27b7
Linghui Luo <linghui@mail.upb.de>
2019-05-28 14:38:02 +0200

    add app

---
+++ src/main/java/magpiebridge/onlineshop/model/product/Comment.java
@@ -0,0 +1,52 @@
+package magpiebridge.onlineshop.model.product;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import magpiebridge.onlineshop.model.AbstractEntity;
+import magpiebridge.onlineshop.model.user.User;
+
+@Entity
+public class Comment extends AbstractEntity {
+
+	private String text;
+
+	@ManyToOne
+	private User user;
+	
+	
+	@ManyToOne
+	private Medication medication;
+
+	public Comment(User user, String text, Medication medication) {
+		setText(text);
+		setUser(user);
+		setMedication(medication);
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Medication getMedication() {
+		return medication;
+	}
+
+	public void setMedication(Medication medication) {
+		this.medication = medication;
+	}
+
+
+}
---
+++ src/main/java/magpiebridge/onlineshop/service/CustomPasswordEncoder.java
@@ -0,0 +1,17 @@
+package magpiebridge.onlineshop.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return rawPassword.equals(encodedPassword);
+	}
+
+}
